4b0192a94f72f54926a4a3f89c6c8c3cba5d45f9
Kento75 <kento2github@gmail.com>
2018-09-12 20:45:17 +0900

    Add function

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Person = (props) => {
   return (
     <div>
-      <p>I'm a {props.name} and I am {props.age} years old!</p>
+      <p>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
     </div>
   )
