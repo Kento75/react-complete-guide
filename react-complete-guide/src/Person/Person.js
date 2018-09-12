6bfcdf7e66eaebd9366b56f00673cfdd886223c0
Kento75 <kento2github@gmail.com>
2018-09-12 21:33:01 +0900

    Add props

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Person = (props) => {
   return (
     <div>
-      <p>I'm {props.name} and I am {props.age} years old!</p>
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   )
 }
