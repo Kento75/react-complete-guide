373db78dd8752736dc05da6a43105401bc516548
Kento75 <kento2github@gmail.com>
2018-09-12 20:22:14 +0900

    Add children

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Person = () => {
+const Person = (props) => {
   return (
-      <p>I'm a Person and I am {Math.floor(Math.random()*30)} years old!</p>
+    <div>
+      <p>I'm a {props.name} and I am {props.age} years old!</p>
+      <p>{props.children}</p>
+    </div>
   )
 }
 
