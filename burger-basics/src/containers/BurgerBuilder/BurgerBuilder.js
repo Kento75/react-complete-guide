9f507f3c8ee2af01dc1c29329ef286d40a87e0bf
Kento75 <kento2github@gmail.com>
2018-09-21 06:28:58 +0900

    Add Burger builder Adding

--- burger-basics/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-basics/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
   state = {
@@ -15,7 +16,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux>
     );
   }
