c20c9aa2542c84a42979632dca319d3a84fd3556
Kento75 <kento2github@gmail.com>
2018-09-24 20:52:21 +0900

    Learn axios create instance

--- http-01-starting-setup/src/index.js
+++ http-01-starting-setup/src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from "axios";
 
+//axios.defaults.headers.common['Authorization'] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
 axios.interceptors.request.use(request => {
   console.log(request);
   // Edit request config
